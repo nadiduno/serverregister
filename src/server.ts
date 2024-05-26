220ff4b070f431953cb9889481dcf733e0782737
nadiduno <nadiduno@gmail.com>
2024-05-26 20:59:02 -0300

    Config REQUEST HTTP

--- src/server.ts
+++ src/server.ts
@@ -6,30 +6,156 @@ const app = fastify()
 
 const prisma = new PrismaClient()
 
+interface UserParams {
+  name: string;
+  lastName: string; 
+  email: string;
+  cpf: number;
+  birthDate: Date;
+  phoneNumber: string;
+  volunteerType: string;
+  crm?: string | null;
+  area?: string | null;
+  state: string;
+  availability: string;
+  notes?: string | null;
+}
+
+//READ
 app.get('/users', async () => {
-  const users = await prisma.user.findMany()
+  try {
+    const users = await prisma.user.findMany();
+    return { users };
+  } catch (error) {
+    console.error('Error when searching for users: ', error);
+    return { message: 'Internal server error' };
+  }
+});
 
-  return { users }
-})
 
+//CREATE
 app.post('/users', async (request, reply) => {
   const createUserSchema = z.object({
     name: z.string(),
+    lastName: z.string(), 
     email: z.string().email(),
-  })
+    cpf: z.number(),
+    birthDate: z.date(),
+    phoneNumber: z.string(),
+    volunteerType: z.string(),
+    crm: z.string().nullable(),
+    area: z.string().nullable(),
+    state: z.string(),
+    availability: z.string(),
+    notes: z.string().nullable(),
+  });
 
-  const { name, email } = createUserSchema.parse(request.body)
+  try {
+    const { name, lastName, email, cpf, birthDate, phoneNumber, volunteerType, crm, area, state, availability, notes } =
+      createUserSchema.parse(request.body);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
+    await prisma.user.create({
+      data: {
+        name,
+        lastName, 
+        email,
+        cpf,
+        birthDate,
+        phoneNumber,
+        volunteerType,
+        crm,
+        area,
+        state,
+        availability,
+        notes,
+      },
+    });
+
+    return reply.status(201).send();
+  } catch (error) {
+    console.error('Erro ao criar usuário: ', error);
+    return reply.status(500).send({ message: 'Erro interno do servidor' });
+  }
+});
+
+//UPDATE
+app.put('/users/:id', async (request, reply) => {
+  const { id } = request.params as { id: string };
+  const {
+    name,
+    lastName,
+    email,
+    cpf,
+    birthDate,
+    phoneNumber,
+    volunteerType,
+    crm,
+    area,
+    state,
+    availability,
+    notes,
+  } = request.body as UserParams;
+
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { id: id },
+    });
+
+    if (!existingUser) {
+      return reply.status(404).send({ message: 'Usuário não encontrado' });
     }
-  })
 
-  return reply.status(201).send()
-})
+    const updatedUser = await prisma.user.update({
+      where: { id: id },
+      data: {
+        name,
+        lastName,
+        email,
+        cpf,
+        birthDate,
+        phoneNumber,
+        volunteerType,
+        crm,
+        area,
+        state,
+        availability,
+        notes,
+      },
+    });
 
+    return reply.status(200).send(updatedUser);
+  } catch (error) {
+    console.error('Erro ao atualizar usuário: ', error);
+    return reply.status(500).send({ message: 'Erro interno do servidor' });
+  }
+});
+
+//DELETE
+app.delete('/users/:id', async (request, reply) => {
+  const { id } = request.params as { id: string };
+
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { id: id },
+    });
+
+    if (!existingUser) {
+      return reply.status(404).send({ message: 'User not found' });
+    }
+
+    const deletedUser = await prisma.user.delete({
+      where: { id: id },
+    });
+
+    return reply.status(200).send(deletedUser);
+  } catch (error) {
+    console.error('Error when deleting user: ', error);
+    return reply.status(500).send({ message: 'Internal server error' });
+  }
+});
+
+
+//SERVER
 app.listen({
   host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333,
