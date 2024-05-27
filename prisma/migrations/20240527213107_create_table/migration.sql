a0a1d719dacd93c3c1b99487ba22bab35508a655
nadiduno <nadiduno@gmail.com>
2024-05-27 18:31:51 -0300

    create migrate new

---
+++ prisma/migrations/20240527213107_create_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" INTEGER NOT NULL,
+    "birthDate" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "volunteerType" TEXT NOT NULL,
+    "crm" TEXT NOT NULL,
+    "area" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "availability" TEXT NOT NULL,
+    "notes" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_cpf_key" ON "User"("cpf");
